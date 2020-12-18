7057cdcae1b4ccd83a4cc9d13929ee91ff8ece99
Mirella <mirellamt03@gmail.com>
2020-12-18 16:44:47 -0300

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,10 @@
+spotify_token = "BQBeoacVimQ9fELMZtHKIMuxPlMQPuz2iIgp_R" \
+                "-mZX9cKp64bDYNg2lmely3z6q2pfHuzfoq1xGHpNH9PRpDIkeYMuzWAD_Vl4ahXY" \
+                "-HBD8esxbXuQPF41tfP1dXrUgLbwJ8FOLJsFZFZdzZxmDrJEoW_MSGhkVIWzEbqi9eOdBRLA-iNADKZoI59RyB" \
+                "-KtzN9zU6Fiiru6bB5hrGSma "
+
+spotify_user_id = "zeldalostwoods"
+base_64 = "YTQ4YjVkNzQ1NGY5NGZkM2EyMDI5M2FkMDA5YWVkOGU6N2ZkMjg4N2I2N2ZkNGNiMGI4NTBhYjMwMTc5ZWY0Y2M="
+
+refresh_token = "AQBdjK0tTBAtLHa8TEyC3T-wkC6A34vvxBCP3bGLXSgCEMAGj_x-kgYpeJ-61u" \
+                "-zO7qhQHfzXdM5aUwvq4KqhllGagB7nF5BaJZxxZDgksP6CzGuKHnJ-0oNOuL7fYBI7l4"
